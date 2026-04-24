171d7d2fd3b649cc670fe02cd51ca3e5607412f5
Khamkhom <Kanjanaroj.Khamkhom@Mattel.com>
2026-04-24 11:58:15 +0700

    405: Remove temp scripts and npm files

--- restore_index.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-const fs = require('fs');
-const content = execSync('git show f49693f:frontend/wwwroot/index.html').toString('utf8');
-fs.writeFileSync('frontend/wwwroot/index.html', content, { encoding: 'utf8' });
-console.log('Done, length:', content.length);
-// Verify
-const check = fs.readFileSync('frontend/wwwroot/index.html', 'utf8');
-const idx = check.indexOf('Loading Jig Inventory');
-console.log('Loading text:', check.substring(idx, idx + 30));
